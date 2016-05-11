8a3ca3ddf477a1a2204d57590664640c316f31c4
Fernando <ffmp777@yahoo.com>
2016-05-11 01:10:14 -0600

    challenge1

---
+++ Section3/Challenge1/backToSchool.php
@@ -0,0 +1,33 @@
+<?php
+
+//Rectangle: length= 10; width= 5.
+$length = 10;
+$width = 5;
+
+$rectangle = $length * $width;
+//echo $rectangle;
+
+//Triangle: base= 12; height= 3.
+$base = 12;
+$height = 3;
+
+$triangle = ($base * $height) / 2;
+//echo $triangle;
+
+//Circle: radius= 5.
+$radius = 5;
+$power = 2;
+
+
+$circle = 3.14 * (pow($radius, $power));
+//echo $circle;
+
+//Trapezium: side a= 10; side b= 8; height= 4.
+$a = 10;
+$b = 8;
+$height = 4;
+
+$trapezium = (($a + $b) / 2) * $height;
+//echo $trapezium;
+
+ ?>
