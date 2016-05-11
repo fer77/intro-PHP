ff9c3aab88213a27449d9c84ace6c2a15857f53e
Fernando <ffmp777@yahoo.com>
2016-05-11 08:35:17 -0600

    if statements

---
+++ Section4/ifStatement.php
@@ -0,0 +1,35 @@
+<?php
+
+/* If statement
+$a = 3;
+$b = 12;
+
+if ($a <= 2 || $b >= 10) {
+  echo "true";
+} */
+
+/* If Else
+$a = 5;
+$b = 6;
+
+if ($a == 5) {
+  echo "a is equal to 5";
+} else {
+  echo "a is not equal to 5";
+} */
+
+/* Else If
+$a = 5;
+
+if ($a % 3 == 0) {
+  echo "The number is a multiple of 3";
+} else if ($a % 4 == 0) {
+  echo "The number is a multiple of 4";
+} else if ($a % 5 == 0) {
+  echo "The number is a multiple of 5";
+} else {
+  echo "The number is not a multiple of 3, 4, or 5";
+} */
+
+
+ ?>
