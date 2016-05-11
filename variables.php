d4709b7c7d8789731838cc03e777bf7a808611f8
Fernando <ffmp777@yahoo.com>
2016-05-10 22:53:40 -0600

    first commit

---
+++ variables.php
@@ -0,0 +1,17 @@
+<?php
+
+$firstName = "Fernando";
+echo $firstName;
+
+echo "<br>";
+
+$a = 1;
+echo "$a";
+
+echo "<br>";
+
+$b = TRUE;
+print "$b"; //Print is similar to echo.
+//True outputs 1.  In PHP 1 represents True and 0 represents False.
+
+ ?>
